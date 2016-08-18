44e77a5220789fd8e2ba7d566332b0bba82d2465
Jonathan <jgibler@gmail.com>
2016-08-18 16:11:04 +0100

    v1

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = module.exports = express();
+app.set('view engine', 'ejs');
+app.use('/js', express.static('js'));
+app.use('/dist', express.static('dist'));
+app.use('/pages', express.static('pages'));
+app.use('/bower_components', express.static('bower_components'));
+
+app.get('/', function (req, res) {
+  res.render(__dirname + '/pages/index.ejs');
+});
+
+app.get('/formas', function (req, res) {
+  res.render(__dirname + '/pages/formas.ejs');
+});
+
+app.get('/documentos', function (req, res) {
+  res.render(__dirname + '/pages/documentos.ejs');
+});
+
+app.get('/login', function (req, res) {
+  res.render(__dirname + '/pages/login.ejs');
+});
+
+//start
+var port = process.argv[2];
+if (port == null) { port = 80;}
+app.listen(port, function () { console.log('Listening on port ' + port); });
